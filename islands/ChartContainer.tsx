0c1f8f3123d12737aed8dd4fafa9f9f7ab6a90a9
Evie Wylie <evie@pop-os.localdomain>
2022-08-09 08:17:22 -0600

    chart container

--- islands/ChartContainer.tsx
+++ islands/ChartContainer.tsx
@@ -9,15 +9,14 @@ import * as d3 from "d3";
 import { Button } from "../components/Button.tsx";
 import { Slider } from '../components/Slider.tsx';
 
-// import { scatterData } from "../Dummy_Data/ScatterPlotChart_data.ts";
+import { scatterData } from "../Dummy_Data/ScatterPlotChart_data.ts";
 import { barData, barLabels } from "../Dummy_Data/BarChart_data.ts";
-import { BarChart } from "https://deno.land/x/d3nodata@v0.0.1.2/charts.ts";
-// import { ScatterPlotChart } from "https://deno.land/x/d3nodata@v0.0.1.2/charts.ts";
+import { BarChart } from "https://deno.land/x/d3nodata@v0.0.1.2.1/charts.ts";
+import { ScatterPlotChart } from "https://deno.land/x/d3nodata@v0.0.1.2.1/charts.ts";
 
 interface ChartProps {
   chart: string;
 }
-
 
 const barChartProperties = {
   data: barData, 
@@ -26,10 +25,9 @@ const barChartProperties = {
   animationDelay: 30,
 }
 
-// const scatterPlotProperties = {
-//   data: scatterData
-// }
-
+const scatterPlotProperties = {
+  data: scatterData
+}
 
 // input: chartProperties are the properties of the chart that the user will be altering
 function ChartDisplay(chart, chartProperties) { 
@@ -66,7 +64,7 @@ function ChartDisplay(chart, chartProperties) {
 
 export default function ChartContainer(){
   
-  const [display, setDisplay] = useState([BarChart, barChartProperties]);
+  const [display, setDisplay] = useState([ScatterPlotChart, scatterPlotProperties]);
 
   function buttonBar() {
     return(
