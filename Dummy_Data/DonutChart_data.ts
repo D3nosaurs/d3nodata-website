5d3e6064550c764d7da92fd5c81531c66a48968e
Evie Wylie <evie@pop-os.localdomain>
2022-08-12 11:12:53 -0600

    dev changes

--- Dummy_Data/DonutChart_data.ts
+++ Dummy_Data/DonutChart_data.ts
@@ -9,13 +9,13 @@
 //   donutData.push({ ages: i, count: Math.floor(Math.random() * 100) });
 // }
 const donutData = [
-  { ages: "a", count: 727432 },
-  { ages: "b", count: 629032 },
-  { ages: "c", count: 515347 },
-  { ages: "d", count: 341435 },
-  { ages: "e", count: 444509 },
-  { ages: "f", count: 426967 },
-  { ages: "g", count: 480565 },
+  { ages: "a", count: "727432" },
+  { ages: "b", count: "629032" },
+  { ages: "c", count: "515347" },
+  { ages: "d", count: "341435" },
+  { ages: "e", count: "444509" },
+  { ages: "f", count: "426967" },
+  { ages: "g", count: "480565" },
 ];
 
 export { donutData };
